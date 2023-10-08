b705c9b14922602e67155595c88a1ddc2e05ed8f
DaVinciX <davincix.007@gmail.com>
2023-10-08 09:50:28 +0800

    init(package-struct): just add file Placeholder to be tracked in git

--- src/main/java/me/davincix/hello_again_java/_001_oop/Placeholder.java
+++ src/main/java/me/davincix/hello_again_java/_001_oop/Placeholder.java
@@ -1,4 +1,7 @@
 package me.davincix.hello_again_java._001_oop;
 
 public class Placeholder {
+    public static void main(String[] args) {
+        System.out.println("hello again");
+    }
 }
--- src/main/java/me/davincix/hello_again_java/whatsnew/_21/Placeholder.java
+++ src/main/java/me/davincix/hello_again_java/whatsnew/_21/Placeholder.java
@@ -1,4 +1,7 @@
 package me.davincix.hello_again_java.whatsnew._21;
 
 public class Placeholder {
+    public static void main(String[] args) {
+        System.out.println("hello java 21");
+    }
 }
